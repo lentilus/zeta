9a45c68cd692f04b26667baccfec8745a6771a61
lentilus <lentilus@mailbox.org>
2024-11-10 12:42:31 +0100

    remmove main

--- aftermath/pkg/cache/cache.go
+++ aftermath/pkg/cache/cache.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
-	"runtime/trace"
 	"sync"
 	"time"
 
@@ -160,29 +158,4 @@ func UpdateIncremental(dir string) {
 
 	// Wait for all goroutines to finish
 	wg.Wait()
-}
-
-func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	// Open a file to store the trace profile
-	f, err := os.Create("trace.out")
-	if err != nil {
-		fmt.Println("could not create trace file:", err)
-		return
-	}
-	defer f.Close()
-
-	// Start trace profiling
-	if err := trace.Start(f); err != nil {
-		fmt.Println("could not start trace:", err)
-		return
-	}
-	defer trace.Stop() // Stop tracing when main exits
-
-	// Directory to walk (replace this with the actual directory you want to test on)
-	dir := "/home/lentilus/typstest"
-
-	// Run the main program logic
-	UpdateIncremental(dir)
 }

510159d85497eefaba540159f7e86a13ea21032a
lentilus <lentilus@mailbox.org>
2025-03-04 21:41:15 +0100

    sketch shared cache interface

---
+++ internal/cache/cache.go
@@ -0,0 +1,30 @@
+package cache
+
+import "time"
+
+type Note string
+
+type Link struct {
+    Reference string
+    source string
+    target string
+    Row int
+    Col int
+}
+
+type ChangeLogEvent struct{}
+
+type Cache interface {
+    UpsertNote(note Note, links []Link) error
+    DeleteNote(note Note) error
+    GetLastModified(note Note) (time.Time, error)
+
+    GetExistingNotes() ([]Note, error)
+    GetMissingNotes() ([]Note, error)
+    GetAllNotes() ([]Note, error)
+
+    GetForwardLinks(source Note) ([]Link, error)
+    GetBackLinks(target Note) ([]Link, error)
+
+    Subscribe() (<-chan ChangeLogEvent, error)
+}

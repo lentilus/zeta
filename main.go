063aeb84a9c51d6e7b07327c3b3b5545ba83fb4b
lentilus <mail@lentilus.me>
2025-04-26 00:21:08 +0200

    feat(main): add logfile cli flag

--- main.go
+++ main.go
@@ -10,45 +10,40 @@ import (
 	"zeta/internal/server"
 
 	"github.com/tliron/commonlog"
+	_ "github.com/tliron/commonlog/simple"
 )
 
 // Version will be set during the build process using ldflags
 var Version = "(dev) v0.0.0"
 
 func main() {
-	// Define and parse the --version flag
 	versionFlag := flag.Bool("version", false, "Print the version of the program")
+	logfileFlag := flag.String("logfile", "", "Path to log file")
 	flag.Parse()
 
-	// Print the version if the flag is set
+	// Version tag
 	if *versionFlag {
 		fmt.Printf("zeta LSP server version %s\n", Version)
 		return
 	}
 
-	// Give it some cores
+	// 4 Cores
 	runtime.GOMAXPROCS(4)
 
-	// Set up logging
-	filename := "/tmp/zeta.log"
-
-	logFile, err := os.OpenFile(
-		filename,
-		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
-		0666,
-	)
-	if err != nil {
-		log.Fatalf("Failed to open log file: %v", err)
+	// Logging
+	if *logfileFlag != "" {
+		logFile, err := os.OpenFile(*logfileFlag, os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("Failed to open log file: %v", err)
+		}
+		defer logFile.Close()
+		log.SetOutput(logFile)
+		log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+		log.Println("Starting zeta LSP server...")
+	} else {
+		log.SetOutput(io.Discard)
 	}
-	defer logFile.Close()
-
-	commonlog.Configure(2, &filename)
-
-	// Set up multi-writer for logging
-	multiWriter := io.MultiWriter(os.Stderr, logFile)
-	log.SetOutput(multiWriter)
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
-	log.Println("Starting zeta LSP server...")
+	commonlog.Configure(2, nil) // Logger used by glsp
 
 	// Initialize the server
 	server, err := server.NewServer()

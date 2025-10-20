2bcecc044cb25a4dbca2b1514ea003ad8028d4a3
lentilus <mail@lentilus.me>
2025-10-20 19:20:07 +0200

    embed interface sketch

---
+++ internal/embed/interface.go
@@ -0,0 +1,15 @@
+package embed
+
+type Embedder interface {
+	// Embed val behind vector embedding of key
+	Embed(key string, val string) error
+	// Delete embedding vector at key
+	Delete(key string) error
+	// Query for values with keys similar to key
+	Query(key string, tol float32) ([]string, []float32, error)
+	// Dump all embeddings for later restauration
+	Dump() []byte
+	// Close frees all resources used by the Embedder
+	Close() error
+}
+
